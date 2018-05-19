14bcbf60af1765615ecffea0c42b9866f355bb80
corneille1234 <tounou1234@gmail.com>
2018-05-19 19:07:21 +0000

    version 1.0

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+    <div id="titre_principal">
+        <img src="IMAGES/logo.jpg" alt="Logo de Sotral" id="logo" />
+        <h1>Societé SOTRAL</h1>
+    </div>
+        <nav>
+            <ul>
+                <li><a href="accueil.php">Accueil</a></li>
+                <li><a href="offre.php">Offre-Operations</a></li>
+                <li><a href="contact.php">Contact</a></li>      
+            </ul>
+        </nav>
+</header>
+            
