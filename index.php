a1605196157c7c86a2c37e64384bf596286c1c93
corneille1234 <tounou1234@gmail.com>
2018-05-19 16:27:44 +0000

    version 1.0

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8"/>
+        <link rel = "stylesheet" href = "dist/css/bootstrap.css" />
+        <link rel = "stylesheet" href = "style.css" />
+        <title>VENTE DE BILLETS EN LIGNE - SOTRAL </title>
+    </head>
+    <body>
+        <div id="bloc_page">
+            <?php include ("header.php"); ?>
+            <div id="banniere_image">
+            </div>  
+            <section>
+                <article>
+                    <h2>NOS PRINCIPALES ACTIVITES</h2>
+                    <h3>La mobilité de toutes les bourses</h3>
+                    
+                        <p>Lorem ipsum dolor sit amet...</p>
+                        <p>Vivamus sed libero nec mauris pulvinar facilisis ut non sem...</p>
+                        <p>Phasellus ligula massa, congue ac vulputate non, dignissim at augue...</p>
+                </article>
+                <aside>
+                    <h1>Qui sommes nous?</h1>
+                    <img src="images/bulle.png" alt="" id="fleche_bulle" />
+                    <p id="photo_zozor"><img src="images/zozor_classe.png" alt="Photo de Zozor" /></p>
+                    <p>La Société de Transport de Lomé (<strong>SOTRAL</strong>),est une société de droit privé constituée sous la forme d’une société 
+                        anonyme avec un actionnariat public et privé et une autonomie de gestion. <strong>La SOTRAL dispose d’une flotte de 88 bus dont 83 
+                        autobus et 5 autocars et exploite dix-huit (18) lignes dont sept (07) lignes dédiées au transport des étudiants de l’université de Lomé et 
+                        onze (11) lignes ordinaires du réseau viaire du grand Lomé.</strong><br> Le réseau de ligne de <strong>SOTRAL</strong> dessert 116 quartiers
+                         et dispose de 736 points d’arrêts dont 30 sont dotés d’abris bus et 706 matérialisés par des panneaux.</p>
+                </aside>
+            </section>   
+            <?php include ("footer.php"); ?>
+        </div>
+    </body>
+</html>
+
