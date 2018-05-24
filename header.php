79aaf88e99066094c6ef9771a4d89ba900df4284
corneille1234 <38997857+corneille1234@users.noreply.github.com>
2018-05-24 18:08:28 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<header>
+    <div id="titre_principal">
+        <img src="IMAGES/logo.jpg" alt="Logo de Sotral" id="logo" />
+        <h1>Societé SOTRAL</h1>
+    </div>
+        <nav class = "navbar navbar-default" role = "navigation">
+        <ol class = "breadcrumb">
+            <li><a href="index.php">Home</a></li>
+            <li>Accueil</li> 
+        </ol>
+        <div class = "navbar-collapse collapse navbar-right"> 
+            <ul class = "nav navbar-nav">
+                <li><a href="index.php"><span class = "glyphicon glyphicon-home"></span> Accueil</a></li>
+                <li><a href="offre.php"><span class = "glyphicon glyphicon-certificate"> Offre-Operations</a></li>
+                <li><a href="contact.php"><span class = "glyphicon glyphicon-inbox"> Contact</a></li>      
+            </ul>
+        </nav>
+</header>
+            
